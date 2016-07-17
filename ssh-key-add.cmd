77b65e61dd65a3388819b1c547dc0eaa4815da44
Michael Schmid <michael@amazeelabs.com>
2016-07-17 21:22:46 +0200

    ssh-key-add.cmd for easier ssh key adding in windows

---
+++ ssh-key-add.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SET KEY=%~f1
+
+IF NOT DEFINED KEY (
+	ECHO %~n0: You need to define the path to the SSH private key as the first parameter >&2
+	EXIT /B 1
+)
+
+IF NOT EXIST "%KEY%" (
+	ECHO %~n0: Given SSH private key "%KEY%" could not be found >&2
+	EXIT /B 1
+)
+
+docker run --rm -it --volume=%KEY%:/key --volumes-from=amazeeio-ssh-agent amazeeio/ssh-agent windows-key-add /key
